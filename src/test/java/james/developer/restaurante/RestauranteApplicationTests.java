1df9ef391b3587cba5bf8dd18f93bad83c9b849c
Tiago Santana <63932833+JamesCode-Ts@users.noreply.github.com>
2023-05-20 19:12:53 -0300

    Update RestauranteApplicationTests.java

--- src/test/java/james/developer/restaurante/RestauranteApplicationTests.java
+++ src/test/java/james/developer/restaurante/RestauranteApplicationTests.java
@@ -1,13 +1,13 @@
-package james.developer.restaurante;
+//package james.developer.restaurante;
 
 //import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class RestauranteApplicationTests {
+//@SpringBootTest
+//class RestauranteApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+	//@Test
+	//void contextLoads() {
+//	}
 
-}
+//}
